1c55ebee9bd744f78f4f3a8c1e80eb1fc01ed4a1
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2022-04-27 14:40:39 +0600

    add todos in user schema

--- Schemas/userSchema.js
+++ Schemas/userSchema.js
@@ -19,6 +19,10 @@ const userSchema = mongoose.Schema({
     status: {
         type: String,
         enum: ['active', 'inactive']
+    },
+    todos: {
+        type: mongoose.Types.ObjectId,
+        ref: "Todo"
     }
 
 });
