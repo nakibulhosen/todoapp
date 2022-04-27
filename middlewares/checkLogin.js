6d2bc73775c3d6967d5c483556a1df59f4b4839c
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2022-04-27 13:12:11 +0600

    fix typo, remove extra console

--- middlewares/checkLogin.js
+++ middlewares/checkLogin.js
@@ -3,12 +3,11 @@ const jwt = require("jsonwebtoken");
 
 const checkLogin = (req, res, next) => {
     const { authorization } = req.headers;
-    console.log('Checking login')
     try {
         const token = authorization.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.username = decoded.username;
-        req.userid = decoded.userid;
+        req.userId = decoded.userid;
         next()
     } catch (error) {
         next('Authentication error. Please Login to continue')
